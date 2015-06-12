fa3c4383a1794dba144a211a535c891b8775ca1d
Richard MacGregor <rmacgregor@cyngn.com>
2015-06-12 12:36:16 -0700

    [WIP] Navigation & homepage
    Move from single view to fragments.
    
    Add homepage.
    The base homepage is basically a list/scroll view with a search box at
    the top. Below will be search quick links and other fast link items.
    
    Change-Id: Ifd5fe9f1859837e1a76e250c42312c35e637657f

---
+++ src/com/cyanogenmod/filemanager/ui/fragments/HomeFragment.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.filemanager.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cyanogenmod.filemanager.R;
+
+public class HomeFragment extends Fragment {
+
+    View mView;
+    Toolbar mToolBar;
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static HomeFragment newInstance() {
+        HomeFragment frag = new HomeFragment();
+        return frag;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.home_fragment, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mToolBar = (Toolbar) mView.findViewById(
+                com.cyanogenmod.filemanager.R.id.material_toolbar);
+
+        ((ActionBarActivity) getActivity()).setSupportActionBar(mToolBar);
+        ((ActionBarActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((ActionBarActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
