61715729ffa5793261285f4b8c4fd14ce860e04f
Richard MacGregor <rmacgregor@cyngn.com>
2015-06-12 12:36:16 -0700

    [WIP] Create base homepage
    The base homepage is basically a list/scroll view with a search box at
    the top. Below will be search quick links and other fast link items.
    
    Change-Id: Ifd5fe9f1859837e1a76e250c42312c35e637657f

--- Android.mk
+++ Android.mk
@@ -16,24 +16,35 @@
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+appcompat_dir := libs/appcompat/res
+supportdesign_dir := libs/support-design/res
+res_dirs := res $(supportdesign_dir) $(appcompat_dir)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_SRC_FILES += $(call all-java-files-under, libs/android-syntax-highlight/src)
 LOCAL_SRC_FILES += $(call all-java-files-under, libs/color-picker-view/src)
 
 LOCAL_STATIC_JAVA_LIBRARIES += libtruezip
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += cmfm-android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += cmfm-android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES += cmfm-android-support-design
 LOCAL_STATIC_JAVA_LIBRARIES += juniversalchardet
 
 LOCAL_PACKAGE_NAME := CMFileManager
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7:android.support.v7.appcompat:android.support.design
 
 include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    juniversalchardet:libs/juniversalchardet/juniversalchardet-1.0.3.jar
+    juniversalchardet:libs/juniversalchardet/juniversalchardet-1.0.3.jar \
+    cmfm-android-support-v4:libs/appcompat/android-support-v4.jar \
+    cmfm-android-support-v7-appcompat:libs/appcompat/android-support-v7-appcompat.jar \
+    cmfm-android-support-design:libs/support-design/android-support-design.jar
 
 include $(BUILD_MULTI_PREBUILT)
 include $(call all-makefiles-under,$(LOCAL_PATH))
