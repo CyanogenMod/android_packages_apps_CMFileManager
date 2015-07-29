c4a456a10be2d5d90ffd481843c3582546c7c6f0
Richard MacGregor <rmacgregor@cyngn.com>
2015-07-29 16:50:28 -0700

    [WIP] Make icons pretty
    New circle icons for all file types.
    Modified navigation view items so icons show up correctly.
    
    TODO:
    - add more file type category icons
    - fix thumbnails
    
    Change-Id: Iab6a31463127d418aa81fafebb5818026b5cc47a

--- src/com/cyanogenmod/filemanager/ui/IconHolder.java
+++ src/com/cyanogenmod/filemanager/ui/IconHolder.java
@@ -21,8 +21,11 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.database.ContentObserver;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
@@ -33,6 +36,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.widget.ImageView;
 
+import com.cyanogenmod.filemanager.R;
 import com.cyanogenmod.filemanager.model.FileSystemObject;
 import com.cyanogenmod.filemanager.ui.ThemeManager.Theme;
 import com.cyanogenmod.filemanager.util.FileHelper;
@@ -55,7 +59,7 @@ public class IconHolder {
     private static final int MSG_LOAD = 1;
     private static final int MSG_LOADED = 2;
 
-    private final Map<String, Drawable> mIcons;     // Themes based
+    private final Map<Integer, IconData> mIcons;     // Themes based
     private final Map<String, Drawable> mAppIcons;  // App based
 
     private Map<String, Long> mAlbums;      // Media albums
@@ -64,10 +68,23 @@ public class IconHolder {
 
     private final Context mContext;
     private final boolean mUseThumbs;
-    private boolean mNeedAlbumUpdate = true;
+
+    private int mDirectoryColor;
 
     private HandlerThread mWorkerThread;
     private Handler mWorkerHandler;
+
+    private static class IconData {
+        Drawable iconDrawable;
+        ColorStateList iconColor;
+        boolean isDir;
+
+        public IconData(Drawable iconDrawable, ColorStateList iconColor, boolean isDir) {
+            this.iconDrawable = iconDrawable;
+            this.iconColor = iconColor;
+            this.isDir = isDir;
+        }
+    }
 
     /**
      * This is kind of a hack, we should have a loadable for each MimeType we run into.
@@ -246,7 +263,7 @@ public class IconHolder {
         this.mContext = context;
         this.mUseThumbs = useThumbs;
         this.mRequests = new WeakHashMap<ImageView, Loadable>();
-        this.mIcons = new HashMap<String, Drawable>();
+        this.mIcons = new HashMap<Integer, IconData>();
         this.mAppIcons = new LinkedHashMap<String, Drawable>(MAX_CACHE, .75F, true) {
             private static final long serialVersionUID = 1L;
             @Override
@@ -261,24 +278,25 @@ public class IconHolder {
                 cr.registerContentObserver(uri, true, mMediaObserver);
             }
         }
+        setVolumeColor(mContext.getResources().getColor(R.color.default_primary));
+        loadDefaultIcons();
     }
 
     /**
      * Method that returns a drawable reference of a icon.
      *
-     * @param resid The resource identifier
+     * @param resId The resource identifier
      * @return Drawable The drawable icon reference
      */
-    public Drawable getDrawable(final String resid) {
+    public Drawable getDrawable(final int resId) {
         //Check if the icon exists in the cache
-        if (this.mIcons.containsKey(resid)) {
-            return this.mIcons.get(resid);
+        if (this.mIcons.containsKey(resId)) {
+            return this.mIcons.get(resId).iconDrawable;
         }
 
         //Load the drawable, cache and returns reference
-        Theme theme = ThemeManager.getCurrentTheme(mContext);
-        Drawable dw = theme.getDrawable(mContext, resid);
-        this.mIcons.put(resid, dw);
+        Drawable dw = mContext.getDrawable(resId);
+        //this.mIcons.put(resId, dw);
         return dw;
     }
 
@@ -290,9 +308,24 @@ public class IconHolder {
      * @param defaultIcon Drawable to be used in case no specific one could be found
      * @return Drawable The drawable reference
      */
-    public void loadDrawable(ImageView iconView, FileSystemObject fso, Drawable defaultIcon) {
+    public void loadDrawable(ImageView iconView, FileSystemObject fso, int defaultIcon) {
+        IconData iconData;
+        if (mIcons.containsKey(defaultIcon)) {
+            iconData = mIcons.get(defaultIcon);
+        } else {
+            iconData = loadIcon(defaultIcon, mContext.getResources().getColor(R.color.category_misc), false);
+        }
         if (!mUseThumbs) {
-            iconView.setImageDrawable(defaultIcon);
+            iconView.setBackgroundResource(R.drawable.ic_icon_background);
+            if (iconData.isDir) {
+                iconView.setBackgroundTintList(iconData.iconColor);
+                iconView.setImageDrawable(iconData.iconDrawable);
+                iconView.setColorFilter(mDirectoryColor, Mode.MULTIPLY);
+            } else {
+                iconView.setBackgroundTintList(iconData.iconColor);
+                iconView.setImageDrawable(iconData.iconDrawable);
+                iconView.setColorFilter(Color.WHITE, Mode.MULTIPLY);
+            }
             return;
         }
 
@@ -315,9 +348,56 @@ public class IconHolder {
 
         Loadable loadable = new Loadable(mContext, iconView, fso);
         mRequests.put(iconView, loadable);
-        iconView.setImageDrawable(defaultIcon);
+        iconView.setBackgroundResource(R.drawable.ic_icon_background);
+        iconView.setBackgroundTintList(iconData.iconColor);
+        iconView.setImageDrawable(iconData.iconDrawable);
 
          mWorkerHandler.obtainMessage(MSG_LOAD, loadable).sendToTarget();
+    }
+    public void loadDrawable(ImageView iconView, FileSystemObject fso, Drawable defaultIcon) {
+        /*IconData iconData;
+        if (mIcons.containsKey(defaultIcon)) {
+            iconData = mIcons.get(defaultIcon);
+        } else {
+            iconData = loadIcon(defaultIcon, mContext.getResources().getColor(R.color.category_misc), false);
+        }
+        if (!mUseThumbs) {
+            iconView.setBackgroundResource(R.drawable.ic_icon_background);
+            if (iconData.isDir) {
+                iconView.setBackgroundTintList(iconData.iconColor);
+                iconView.setImageDrawable(iconData.iconDrawable);
+                //itemIcon.setColorFilter(mDirectoryColor, Mode.MULTIPLY);
+            } else {
+                iconView.setBackgroundTintList(iconData.iconColor);
+                iconView.setImageDrawable(iconData.iconDrawable);
+            }
+            return;
+        }
+
+        // Is cached?
+        final String filePath = MediaHelper.normalizeMediaPath(fso.getFullPath());
+        if (this.mAppIcons.containsKey(filePath)) {
+            iconView.setImageDrawable(this.mAppIcons.get(filePath));
+            return;
+        }
+
+        if (mWorkerThread == null) {
+            mWorkerThread = new HandlerThread("IconHolderLoader");
+            mWorkerThread.start();
+            mWorkerHandler = new WorkerHandler(mWorkerThread.getLooper());
+        }
+        Loadable previousForView = mRequests.get(iconView);
+        if (previousForView != null) {
+            mWorkerHandler.removeMessages(MSG_LOAD, previousForView);
+        }
+
+        Loadable loadable = new Loadable(mContext, iconView, fso);
+        mRequests.put(iconView, loadable);
+        iconView.setBackgroundResource(R.drawable.ic_icon_background);
+        iconView.setBackgroundTintList(iconData.iconColor);
+        iconView.setImageDrawable(iconData.iconDrawable);
+
+        mWorkerHandler.obtainMessage(MSG_LOAD, loadable).sendToTarget();*/
     }
 
     private class WorkerHandler extends Handler {
@@ -358,5 +438,59 @@ public class IconHolder {
         this.mAppIcons.clear();
         mContext.getContentResolver().unregisterContentObserver(mMediaObserver);
         shutdownWorker();
+    }
+
+    /**
+     * Method that sets the colors to use for the current volumes directories
+     */
+    public void setVolumeColor(int color) {
+        mDirectoryColor = color;
+        float opacity =
+                mContext.getResources().getFloat(R.float.navigation_view_icon_circle_opacity);
+        int transparentColor = Color.argb(
+                Math.round(((float)0xFF) * opacity),
+                Color.red(color),
+                Color.green(color),
+                Color.blue(color));
+        loadIcon(R.drawable.ic_folder, transparentColor, true);
+    }
+
+    /**
+     * Method that loads the default icons (known icons and more common icons).
+     */
+    private void loadDefaultIcons() {
+        loadIcon(R.drawable.ic_category_apps,
+                mContext.getResources().getColor(R.color.category_apps), false);
+        loadIcon(R.drawable.ic_category_archives,
+                mContext.getResources().getColor( R.color.category_archives), false);
+        loadIcon(R.drawable.ic_category_audio,
+                mContext.getResources().getColor(R.color.category_audio), false);
+        loadIcon(R.drawable.ic_category_docs,
+                mContext.getResources().getColor(R.color.category_docs), false);
+        loadIcon(R.drawable.ic_category_images,
+                mContext.getResources().getColor(R.color.category_images), false);
+        loadIcon(R.drawable.ic_category_misc,
+                mContext.getResources().getColor(R.color.category_misc), false);
+        loadIcon(R.drawable.ic_category_video,
+                mContext.getResources().getColor(R.color.category_video), false);
+
+        // Icon selected state
+        loadIcon(R.drawable.ic_check,
+                mContext.getResources().getColor(R.color.navigation_view_icon_selected), false);
+    }
+
+    private IconData loadIcon(int resId, int color, boolean isDir) {
+        //Check if the icon exists in the cache
+        if (mIcons.containsKey(resId)) {
+            mIcons.remove(resId);
+        }
+
+        //Load the drawable, cache and returns reference
+        Drawable dw = mContext.getDrawable(resId);
+        ColorStateList colorList = new ColorStateList(new int[][]{new int[]{}},
+                new int[]{color});
+        IconData iconData = new IconData(dw, colorList, isDir);
+        mIcons.put(resId, iconData);
+        return iconData;
     }
 }
