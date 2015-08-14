f95d860238a9ca0aff16bc7129c91cd0c25b6f0a
Hayden Schoen <hayden@cyngn.com>
2015-08-14 13:46:09 -0700

    Create Welcome file manager experience
    Change-Id: I3d713e44249680f826c91df10214c50b9f2372d9

---
+++ src/com/cyanogenmod/filemanager/controllers/ScrollerCustomDuration.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.filemanager.controllers;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
+
+/**
+ * Custom scroller for custom viewpager
+ */
+public class ScrollerCustomDuration extends Scroller {
+
+    private double mScrollFactor = 1;
+
+    public ScrollerCustomDuration(Context context) {
+        super(context);
+    }
+
+    public ScrollerCustomDuration(Context context, Interpolator interpolator) {
+        super(context, interpolator);
+    }
+
+    @SuppressLint("NewApi")
+    public ScrollerCustomDuration(Context context, Interpolator interpolator, boolean flywheel) {
+        super(context, interpolator, flywheel);
+    }
+
+    /**
+     * Set the factor by which the duration will change
+     */
+    public void setScrollDurationFactor(double scrollFactor) {
+        mScrollFactor = scrollFactor;
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+        super.startScroll(startX, startY, dx, dy, (int) (duration * mScrollFactor));
+    }
+
+}
